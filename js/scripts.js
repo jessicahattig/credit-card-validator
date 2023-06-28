4949229c5868056f3e28c3d47c7162189d5b5a63
jessicahattig <jessicahattig@gmail.com>
2023-06-27 19:57:05 -0700

    TDD: second test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
- function cardVerify(cardNumber){
-    let parseNumber = parseInt(cardNumber);
+ function cardVerify(cardNumber){ 
   if (cardNumber.length === 16) {
+   // let parseNumber = parseInt(cardNumber);
     console.log("This card number is valid");
     return true;
   } else {
+    console.log("This card number is not valid" )
     return false;
     }
   };
