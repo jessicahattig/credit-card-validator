a9428828a2b30ec0f1f16e52ee1e474647bd147a
jessicahattig <jessicahattig@gmail.com>
2023-06-28 20:37:46 -0700

    WIP: Second function tests.

--- TESTS.md
+++ TESTS.md
@@ -1,5 +1,17 @@
 Description: cardVerify()
 
+Test: It should reject inputs which aren't numbers:
+code: 
+let card = "hello";
+cardVerify();
+Expected Output: "hello is not a number."
+
+Test: It should reject inputs which are numbers:
+code: 
+let card = "123";
+cardVerify();
+Expected Output: "123 is a number."
+
 Test: It should return true if the input is 16 numbers."
 Code:
 let card = "0998445533334452";
@@ -14,11 +26,18 @@ Expected Output:"This card number is not valid."
 
 Description: luhnAlg()
 
-Test: "It should create a new set of numbers by transforming each digit in the inputted card number."
-Code:
-let card = "4102 0808 8043 5620";
+Test: "It should return the inputted card number."
+Code: 
+let cardNumber = "4102 0808 8043 5620";
 luhnAlg();
-Expected Output: "4204 0707 8046 5320"
+Expected Output: ""4102 0808 8043 5620";
+
+
+Test: "It should return every other digit of card number."
+Code:
+let cardNumber = "4102 0808 8043 5620";
+luhnAlg();
+Expected Output: "12880360"
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,69 @@
     }
   };
 
-function luhnAlg(cardNumber) {
-  let arrX = cardNumber.split(" ");
-  let newCard = arrX.map(function(number, index) {
-    if (index % 2 === 1) {
-      return (parseInt(number) * 2).toString();
-    } else {
-    return number;
+  let cardNumber = "hello"
+  function numberVer(cardNumber) {
+    if (isNaN(cardNumber)) {
+      return cardNumber + " is not a number."
+      return false;
+    } else if (cardNumber.length === 16) {
+      return cardNumber + "is a valid credit card number."
+      return true;
     }
+  }
+
+
+let cardNumber = "16468534223";
+luhnAlg(cardNumber) {
+  let digits = Array.from(cardNumber);
+  let arrayEven = digits.map((x, index) => {
+    return i % 2 === 0;
   });
-console.log(newCard);
-return newCard.join(" ");
+};
+console.log(luhnAlg(cardNumber));
+
+  let newCard = [];
+
+  }
+  for (let i = 0; i < digits.length; i++) {
+      if (i % 2 === 0) {
+        newCard.push(digits);
+        } 
+      }
+  return newCard;
 }
+
+
+function luhnAlg(cardNumber) {
+  let digits = Array.from(cardNumber);
+
+
+
+
+
+
+let cardNumber = "4102080880435620";
+let everyOtherDigit= luhnAlg(cardNumber);
+console.log 
+
+
+let evenNum = arrX.filter((element, index) => {
+  return index % 3 === 0;
+}
+
+
+// function luhnAlg(cardNumber) {
+//   let arrX = cardNumber.split(" ");
+//   let newCard = arrX.map(function(number, index) {
+//     if (index % 2 === 1) {
+//       return (parseInt(number) * 2).toString();
+//     } else {
+//     return number;
+//     }
+//   });
+// console.log(newCard);
+// return newCard.join(" ");
+// }
 
 
 
